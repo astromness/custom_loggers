bf430247706ae37d0ca5470315c23692597f96bd
austin.stromness <astromness@live.com>
2021-04-28 15:40:51 -0600

    Added release to download_url and updated version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'custom_loggers',
   packages = ['custom_loggers'],
-  version = '1.0',
+  version = '1.0.0',
   license='MIT',
   description = 'A Custom Logger Class for creating colored, adding level, other misc features',
   author = 'Austin Stromness',
   author_email = 'stromnessdevelopment@gmail.com',
   url = 'https://github.com/astromness/custom_loggers',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['logging', 'colored', 'loglevel', 'console'],
+  download_url = 'https://github.com/astromness/custom_loggers/archive/refs/tags/1.0.0.tar.gz',    # I explain this later on
+  keywords = ['logging', 'colored', 'loglevel', 'console', 'custom'],
   install_requires=[],
   classifiers=[
     'Development Status :: 5 - Production/Stable',
