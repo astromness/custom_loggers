562870991c04435109c5a97b2e80a38f7a1c7110
austin.stromness <astromness@live.com>
2021-04-28 10:34:54 -0600

    Initial Commit Version1.0

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'custom_loggers',
+  packages = ['custom_loggers'],
+  version = '1.0',
+  license='MIT',
+  description = 'A Custom Logger Class for creating colored, adding level, other misc features',
+  author = 'Austin Stromness',
+  author_email = 'stromnessdevelopment@gmail.com',
+  url = 'https://github.com/astromness/custom_loggers',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['logging', 'colored', 'loglevel', 'console'],
+  install_requires=[
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Debugging :: Logging',
+    'License :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
